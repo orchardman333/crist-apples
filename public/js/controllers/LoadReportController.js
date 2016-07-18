91e4674db9b46492e4e1e2aafc607f2a069be936
Scott Ross <sr523@cornell.edu>
2016-07-17 23:11:24 -0400

    added smart-table

--- public/js/controllers/LoadReportController.js
+++ public/js/controllers/LoadReportController.js
@@ -4,5 +4,19 @@ angular.module('crist_farms')
 
 .controller('LoadReportController', ['$scope',
  function ($scope) {
+     //rowCollection
+     var rowCollection = [];
+     var person = {
+       lastName: 'Ross',
+       firstName: 'Scott'
+     }
 
+     var person2= {
+       lastName: 'Crist',
+       firstName: 'Jedd'
+     }
+     rowCollection.push(person);
+     rowCollection.push(person2);
+
+     $scope.rowCollection=rowCollection;
  }]);
