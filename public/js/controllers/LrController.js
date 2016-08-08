0eb2f51821d79bdcc649f375a251d38eda6368c4
Scott Ross <sr523@cornell.edu>
2016-08-07 23:26:26 -0400

    fixed issue 25

--- public/js/controllers/LrController.js
+++ public/js/controllers/LrController.js
@@ -8,13 +8,29 @@ angular.module('crist_farms')
      var barCodes = loadRunService.GetData().barCodes;
      for(var i=0; i < barCodes.length; i++)
      {
-       console.log(barCodes[i]);
-       $scope.rowCollection.push({
-        bin_id: barCodes[i].bin_id,
-        block_names: barCodes[i].block_names,
-        variety: barCodes[i].variety,
-        storage: barCodes[i].storage.id,
-        truck_driver: barCodes[i].truck_driver.name
-      });
+       var eeList = [];
+       for(var index=i+1; index < barCodes.length; index++)
+       {
+         if (barCodes[index].barcode.length < 13){
+           eeList.push(barCodes[index].barcode);
+         }
+         else {
+           break;
+         }
+       }
+
+       if (barCodes[index].barcode.length < 13)
+       {
+         $scope.rowCollection.push({
+          bin_id: barCodes[i].bin_id,
+          block_names: barCodes[i].block_names,
+          variety: barCodes[i].variety,
+          storage: barCodes[i].storage.id,
+          truck_driver: barCodes[i].truck_driver.name,
+          nr_boxes : barCodes[i].nr_boxes,
+          comments: barCodes[i].comments,
+          pickers : eeList.join(",")
+        });
+       }
      }
  }]);
