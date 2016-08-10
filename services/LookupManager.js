e88d683e61918f7b89ae1e9b9a3925f2a8cb200f
Scott Ross <sr523@cornell.edu>
2016-08-10 19:43:34 -0400

    addressed issue with #32 with block name being null

--- services/LookupManager.js
+++ services/LookupManager.js
@@ -40,12 +40,16 @@ var doWork = function(varietyId,strainId, blockId, callback ){
       }
       conn().query("select `Block Name` as name from block_table where `Block Id` ='" + blockId +"'", function(err, t, fields) {
         console.log(blockId);
-        if (t.length == 1)
-        {
-          block = t[0].name;
-        }
-        else {
+        if t == null{
           block='';
+        }else {
+          if (t.length == 1)
+          {
+            block = t[0].name;
+          }
+          else {
+            block='';
+          }
         }
         callback({varietyName: varietyName,strainName:strain,blockName:block});
       });
