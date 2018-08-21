efa24db042ce55a15e8e42fa60ef1f7258633411
jedd <jedd@cristapples.com>
2018-08-21 10:16:36 -0400

    orchard run report printable report

--- public/js/controllers/OrchardRunReportController.js
+++ public/js/controllers/OrchardRunReportController.js
@@ -9,4 +9,19 @@ function ($scope, orchardRunService) {
   $scope.getBushelSum = function(items) {
     return items.map(a=>a.bushels).reduce((b,c)=>b+c,0);
   };
+
+  $scope.downloadPDF = function() {
+    var doc = new jsPDF('landscape');
+
+    var res1 = doc.autoTableHtmlToJson(document.getElementById("header-table"));
+    doc.autoTable(res1.columns, res1.data, {startY: 20, theme: 'plain'});
+    let first = doc.autoTable.previous;
+
+    var res2 = doc.autoTableHtmlToJson(document.getElementById("bins-table"));
+    doc.autoTable(res2.columns, res2.data, {startY: first.finalY + 5, theme: 'plain'});
+    
+    document.getElementById("print").data = doc.output('datauristring');
+    doc.autoPrint();
+    doc.save($scope.load.loadData.load.id);
+  }
 }]);
